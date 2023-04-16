ac2a6662d53cbe3441fdf51ce43741765f38baf7
Jay2182 <jaykanjia11@gmail.com>
2023-04-16 12:32:28 +0530

    local db to online db

--- db/config.js
+++ db/config.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
+// mongoose
+// 	.connect(process.env.DB_URL)
+// 	.then(() => {
+// 		console.log("Server connected to database");
+// 	})
+// 	.catch((err) => {
+// 		console.log("error during database connection " + err);
+// 	});
+
 mongoose
-    .connect(process.env.DB_URL)
-    .then(() => {
-        console.log("Server connected to database");
-    })
-    .catch((err) => {
-        console.log("error during database connection");
-    });
+	.connect(process.env.ATLAS_DB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("db connected");
+	})
+	.catch((err) => {
+		console.log("error during database connection");
+	});
